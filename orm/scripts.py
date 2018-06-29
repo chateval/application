b2c93f4f5e74ab1066fbc5b45a57876d362a91f2
arun <arun.kirubarajan@gmail.com>
2018-06-29 11:01:52 -0400

    Remove unnecessary length prints

--- orm/scripts.py
+++ orm/scripts.py
@@ -18,7 +18,6 @@ def get_messages(model_id, evalset_id):
 
 def upload_responses(responses, dataset, model, submission):
     prompts = EvaluationDatasetText.objects.filter(evaluationdataset=dataset)
-    print(len(prompts), len(responses))
     model_responses = list()
     for i in range(min(len(responses), len(prompts))):
         model_response = ModelResponse(model_submission=submission, evaluationdataset=dataset, 

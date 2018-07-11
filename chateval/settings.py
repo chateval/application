5b627d0806c60c6cebd4ebe97f3d61274209da8f
arun <arun.kirubarajan@gmail.com>
2018-07-10 23:13:56 -0400

    Add static settings

--- chateval/settings.py
+++ chateval/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/chateval/static/'
+STATIC_ROOT = '/chateval/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))

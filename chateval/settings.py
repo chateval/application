7ad3e4b271f0dceae90b23a3e0e292ea5caafcc6
arun <arun.kirubarajan@gmail.com>
2018-07-11 00:35:48 -0400

    Update static URL

--- chateval/settings.py
+++ chateval/settings.py
@@ -127,8 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-STATIC_URL = os.path.join(SITE_ROOT, 'static/')
-
+STATIC_URL = "https://storage.googleapis.com/kirubarajan-site.appspot.com/chateval/"
 STATICFILES_DIRS = [
     os.path.join(SITE_ROOT, 'assets/'),
 ]

ce5083b0c9aa91e315d04c3a2ca162bac15538cc
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-07-18 12:54:22 -0400

    Clean up UI

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def splash(request):
     return render(request, 'splash.html', {'datasets': datasets, 'baselines': baselines, 'metrics': metrics})
 
 def conversations(request):
-    models = Model.objects.all()
+    models = Model.objects.filter(archived=False)
     datasets = EvaluationDataset.objects.all()
     if request.GET.get('model_id') is not None and request.GET.get('evalset_id') is not None:
         messages = get_messages(request.GET.get('model_id'), request.GET.get('evalset_id'))
@@ -19,7 +19,7 @@ def conversations(request):
                                             'models': models, 'datasets': datasets})
 
 def model(request):
-    models = Model.objects.all()
+    models = Model.objects.filter(archived=False)
     if request.GET.get('model_id') is not None and request.GET.get('model_id') is not None:
         messages = get_messages(request.GET.get('model_id'), request.GET.get('evalset_id'), get_all=False)
         submission = ModelSubmission.objects.filter(model=request.GET.get('model_id'))[0]

4f9a09411e0b4c0587131e49853212812b9881fb
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2018-09-12 10:32:07 -0400

    Fix uploading

--- eval/scripts/upload_model.py
+++ eval/scripts/upload_model.py
@@ -6,25 +6,23 @@ from json import dumps
 from boto3 import session
 from django.shortcuts import redirect
 from orm.models import Model, EvaluationDatasetText, ModelResponse, ModelSubmission, EvaluationDataset, AutomaticEvaluation, Metric
-from orm.scripts import get_baselines, get_messages
+from orm.scripts import get_latest_baseline, get_messages
 from chateval.settings import (AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_STORAGE_BUCKET_LOCATION)
 
 def handle_submit(model, dataset, response_file, is_baseline):
-    ## perform checks/retrieve data
     if not is_baseline:
-        baseline = get_baselines(dataset.evalset_id)[0]
+        baseline = get_latest_baseline(dataset.evalset_id)
         baseline_responses = [message['response'] for message in get_messages(baseline, dataset.evalset_id)]
     else:
         baseline_responses = list()
-
-    ## running automatic evaluations
+        
     responses = response_file.file.getvalue().decode(encoding='UTF-8').split('\n')
     evaluations = requests.post(os.environ['EVAL_LOCATION'], json=dumps({'model_responses': responses, 'baseline_responses': baseline_responses, 'is_baseline': is_baseline})).json()
 
-    ## saving everything to database
     model.save()
     submission = ModelSubmission(model=model, date=datetime.datetime.now().date())
     submission.save()
+    submission.evaluationdatasets.add(dataset)
     if is_baseline:
         dataset.baselines.add(model)
         dataset.save()
@@ -57,4 +55,5 @@ def save_evaluations(evaluations, dataset, model, submission, is_baseline):
         auto_evaluations.append(AutomaticEvaluation(metric=Metric.objects.get(metric_id=6), model=model, evaluationdataset=dataset, value=evaluations['average_embedding_score'], model_submission=submission))
         auto_evaluations.append(AutomaticEvaluation(metric=Metric.objects.get(metric_id=4), model=model, evaluationdataset=dataset, value=evaluations['greedy_match'], model_submission=submission))
         auto_evaluations.append(AutomaticEvaluation(metric=Metric.objects.get(metric_id=5), model=model, evaluationdataset=dataset, value=evaluations['extrema_score'], model_submission=submission))
+    
     AutomaticEvaluation.objects.bulk_create(auto_evaluations)

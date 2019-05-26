b7734f4e9456363918b4931d3701f61508492b84
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2019-05-26 12:04:04 -0700

    Fix merge conflicts

--- ui/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-  <footer class="footer">
-    <div class="container">
-      <span class="text-muted">Built by the ChatEval Team</span>
-    </div>
-  </footer>
-);
-    
-  export default Footer;

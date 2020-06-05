69261977706bf85bc61908e8ae19e0d7c291e836
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-05 11:09:29 -0400

    Add Evaluation Dataset to DRF API

--- orm/urls.py
+++ orm/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from orm.views import welcome, ModelList, BaselineList
+from orm.views import welcome, ModelList, BaselineList, EvaluationDatasetList
 
 
 urlpatterns = [
     path('', welcome),
     path('model', ModelList.as_view(), name='model-list'),
-    path('baseline', BaselineList.as_view(), name='baseline-list')
+    path('baseline', BaselineList.as_view(), name='baseline-list'),
+    path('evaluation-dataset', EvaluationDatasetList.as_view(), name='evaluation-dataset-list')
 ]

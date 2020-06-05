13ae6444ddd9487d4887668795f62a4d4e47c295
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-05 11:21:41 -0400

    Add model detail view

--- orm/urls.py
+++ orm/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from orm.views import welcome, ModelList, BaselineList, EvaluationDatasetList
+from orm.views import welcome, ModelList, ModelDetail, BaselineList, EvaluationDatasetList
 
 
 urlpatterns = [
     path('', welcome),
     path('model', ModelList.as_view(), name='model-list'),
+    path('model/<int:pk>', ModelDetail.as_view(), name='model-detail'),
     path('baseline', BaselineList.as_view(), name='baseline-list'),
     path('evaluation-dataset', EvaluationDatasetList.as_view(), name='evaluation-dataset-list')
 ]

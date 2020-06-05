b9eb4978ff8b4c41acb37008903eb7ab9ed5e15d
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-05 13:11:14 -0400

    Add automatic evaluation DRF API routes

--- orm/views.py
+++ orm/views.py
@@ -2,21 +2,22 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
-from orm.models import Model, EvaluationDataset
-from orm.serializers import ModelSerializer, EvaluationDatasetSerializer
+from orm.models import Model, EvaluationDataset, AutomaticEvaluation
+from orm.serializers import (
+    ModelSerializer, 
+    EvaluationDatasetSerializer, 
+    AutomaticEvaluationSerializer
+)
 
 
 @api_view(['GET'])
 def welcome(request, format=None):
     return Response({
         'model': reverse('model-list', request=request, format=format),
-        'baseline': reverse('baseline-list', request=request, format=format)
+        'baseline': reverse('baseline-list', request=request, format=format),
+        'evaluation-dataset': reverse('evaluation-dataset-list', request=request, format=format),
+        'automatic-evaluation': reverse('automatic-evaluation-list', request=request, format=format)
     })
-
-
-@api_view(['POST'])
-def upload(request):
-    pass
 
 
 class ModelList(generics.ListCreateAPIView):
@@ -37,3 +38,13 @@ class BaselineList(generics.ListCreateAPIView):
 class EvaluationDatasetList(generics.ListCreateAPIView):
     queryset = EvaluationDataset.objects.all()
     serializer_class = EvaluationDatasetSerializer
+
+
+class AutomaticEvaluationList(generics.ListAPIView): 
+    serializer_class = AutomaticEvaluationSerializer
+
+    def get_queryset(self):
+        return AutomaticEvaluation.objects.filter(
+            evaluationdataset=self.request.query_params.get('evaluationdataset_id'),
+            model=self.request.query_params.get('model_id')
+        )

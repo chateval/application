5fb707f2eedd4f595d3fe2455af7117dba81b1d9
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-05 13:11:14 -0400

    Add automatic evaluation DRF API routes

--- orm/urls.py
+++ orm/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from orm.views import welcome, ModelList, ModelDetail, BaselineList, EvaluationDatasetList
+from orm.views import (
+    welcome, 
+    ModelList, 
+    ModelDetail, 
+    BaselineList, 
+    EvaluationDatasetList,
+    AutomaticEvaluationList
+)
 
 
 urlpatterns = [
@@ -7,5 +14,6 @@ urlpatterns = [
     path('model', ModelList.as_view(), name='model-list'),
     path('model/<int:pk>', ModelDetail.as_view(), name='model-detail'),
     path('baseline', BaselineList.as_view(), name='baseline-list'),
-    path('evaluation-dataset', EvaluationDatasetList.as_view(), name='evaluation-dataset-list')
+    path('evaluation-dataset', EvaluationDatasetList.as_view(), name='evaluation-dataset-list'),
+    path('automatic-evaluation', AutomaticEvaluationList.as_view(), name='automatic-evaluation-list'),
 ]

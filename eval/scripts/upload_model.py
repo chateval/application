60840efbb3ee29e0bbaaca4231cd89fa9faa909b
Joao Sedoc <jsedoc@sedoc-nyu-mbp.myfiosgateway.com>
2021-09-17 07:50:09 -0400

    submit file zip

--- eval/scripts/upload_model.py
+++ eval/scripts/upload_model.py
@@ -74,7 +74,7 @@ def upload_dstc10_file(path, body):
     s3 = session.resource('s3')
     fileobj = gzip.GzipFile(fileobj=body, mode='rb')
     path = path + ".gz"
-    if s3.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key=path, Body=fileobj):
+    if s3.upload_fileobj(fileobj, AWS_STORAGE_BUCKET_NAME, path):
         return True
     else:
         return False

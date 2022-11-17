c7d17a1c4b63e5fbc7707cd79bfadbf0b4faf5e5
Joao Sedoc <jsedoc@10-16-105-22.dynapool.wireless.nyu.edu>
2022-11-16 19:24:12 -0500

    fixing change for dstc11

--- chateval/urls.py
+++ chateval/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-
-from eval.views import uploads, submit, delete, publish, human, login_view, signup_view, compare, dbdc5download, dstc10download, dbdc5submit, dstc10submit
+from eval.views import uploads, submit, delete, publish, human, login_view, signup_view, compare, dbdc5download, dstc10download, dbdc5submit, dstc10submit, dstc11submit
 from orm.api import api, responses, evaluationdatasets, prompts, models, automatic_evaluations, human_evaluations, baselines, metrics, model as api_model
-# old ## from eval.views import uploads, submit, delete, publish, human, login_view, signup_view, compare, dbdc5download, dbdc5submit
-# from orm.api import api, responses, evaluationdatasets, prompts, models, automatic_evaluations, human_evaluations, baselines, metrics, model as api_model
 
 
 urlpatterns = [
@@ -15,7 +12,16 @@ urlpatterns = [
     path('accounts/signup/', signup_view, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     # API ROUTES
-    path('api/', include('orm.urls')),
+    path('api/automatic_evaluations', automatic_evaluations, name='automatic_evaluations'),
+    path('api/human_evaluations', human_evaluations, name='human_evaluations'),
+    path('api/models', models, name='models'),
+    path('api/model', api_model, name='model'),
+    path('api/baselines', baselines, name='baselines'),
+    path('api/evaluationdatasets', evaluationdatasets, name='evaluationdatasets'),
+    path('api/prompts', prompts, name='prompts'),
+    path('api/metrics', metrics, name='metrics'),
+    path('api/responses', responses, name='responses'),
+    path('api/', api, name='api'),
     ### ACTIONS
     path('model/delete/', delete, name='delete'),
     path('model/publish/', publish, name='publish'),
